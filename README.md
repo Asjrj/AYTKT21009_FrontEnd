d94b60220ec0c005a76575b9bd1a2c703b465776
Antti Penttinen <asjrj200@gmail.com>
2018-10-24 14:47:36 +0300

    Tehtävä 5.1

---
+++ README.md
@@ -0,0 +1,2 @@
+## bloglist frontend
+AYTKT21009 Tehtävät 5.1. => 
---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      blogs: [],
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    blogService.getAll().then(blogs =>
+      this.setState({ blogs })
+    )
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+      this.setState({ username: '', password: '', user })
+    } catch (exception) {
+      this.setState({
+        error: 'Invalid username or password',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleUsernameChange = (event) => {
+    this.setState({ username: event.target.value })
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+
+  render() {
+    if (this.state.user === null) {
+      return (
+        <div>
+          <h2>Log in to application</h2>
+          <form onSubmit={this.login}>
+            <div>
+              username:
+            <input
+                type="text"
+                value={this.state.username}
+                onChange={this.handleUsernameChange}
+              />
+            </div>
+            <div>
+              password:
+            <input
+                type="password"
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>Blogs</h2>
+          <p>{this.state.user.name} logged in</p>
+          {this.state.blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
