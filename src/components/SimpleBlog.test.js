b4cab361991cda16149cda13afec30b85bf09a61
Antti Penttinen <asjrj200@gmail.com>
2018-10-29 13:13:19 +0200

    Tehtävä 5.13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -9,26 +9,31 @@ describe.only('<SimpleBlog />', () => {
     author: 'Kirjoittaja',
     likes: 2
   }
+  const mockHandler = jest.fn()
 
-  const blogComponent = shallow(<SimpleBlog blog={blog} />)
+  const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
   console.log('*** blogComponent:', blogComponent.debug())
-
   const blogLineDiv = blogComponent.find('.BlogLine')
   console.log('*** blogLineDiv:', blogLineDiv.debug())
-
-  const likesDiv = blogComponent.find('#likesDiv')
-  console.log('*** likesDiv:', likesDiv.debug())
-
   it('title is rendered', () => {
     expect(blogLineDiv.text()).toContain(blog.title)
   })
-
   it('author is rendered', () => {
     expect(blogLineDiv.text()).toContain(blog.author)
   })
 
+  const likesDiv = blogComponent.find('#likesDiv')
+  console.log('*** likesDiv:', likesDiv.debug())
   it('likes is rendered', () => {
     expect(likesDiv.text()).toContain(blog.likes)
+  })
+
+  const button = blogComponent.find('button')
+  console.log('*** button', button.debug())
+  button.simulate('click')
+  button.simulate('click')
+  it('like -button is clicked twice', () => {
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 
 })
